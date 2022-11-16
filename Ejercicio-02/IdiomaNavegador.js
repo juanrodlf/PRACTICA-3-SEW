4a7f97f5b4c15d5680aba4047f9f266a41dff7d5
uo271773 <uo271773@uniovi.es>
2022-11-16 19:19:22 +0100

    Initial commit

---
+++ Ejercicio-02/IdiomaNavegador.js
@@ -0,0 +1,3 @@
+/* IdiomaNavegador.js */
+// Version 1.0 08/11/2021 
+document.write(infoNavegador.idioma);
---
+++ Ejercicio-02/NombreNavegador.js
@@ -0,0 +1,3 @@
+/* NombreNavegador.js */
+// Version 1.0 08/11/2021 
+document.write(infoNavegador.nombre);
